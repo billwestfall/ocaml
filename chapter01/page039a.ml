28a9d6ed4b4feca24d99296ace3dc20225f93d7f
billwestfall <billwestfall@yahoo.com>
2015-10-08 12:25:38 -0700

    Update page039a.ml
    adding a more generic function for reference from https://ocaml.org/learn/tutorials/structure_of_ocaml_programs.html

--- chapter01/page039a.ml
+++ chapter01/page039a.ml
@@ -1,10 +1,18 @@
 (*
 # #use "page039a.ml";;
 val f : float -> float -> float = <fun>
+val average : float -> float -> float = <fun>
 # f 3.0 5.1;;
 - : float = 73.71
+# average 2.5 3.5;;
+- : float = 3.
+# average 9.1 8.3;;
+- : float = 8.7
 *)
 
 let f a b =
   let x = a +. b in
     x +. x ** 2.;;
+
+let average a b =
+    (a +. b) /. 2.0;;
