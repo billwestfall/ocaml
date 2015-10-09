789e8d0c6da23c01ab43717f0df086e87b2cc1e0
billwestfall <billwestfall@yahoo.com>
2015-10-08 18:17:42 -0700

    Create page055a.ml

---
+++ chapt02/page055a.ml
@@ -0,0 +1,12 @@
+(*
+# #use "page055a.ml";;
+val double : int -> int = <fun>
+val triple : int -> int = <fun>
+# double 5;;
+- : int = 10
+# triple 4;;
+- : int = 12
+*)
+
+let double x = x + x;;
+let triple x = 3 * x;;
