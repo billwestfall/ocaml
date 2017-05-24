9c35c5b6746d001e205f8bc062ef74507aea7000
Bill Westfall <billwestfall@yahoo.com>
2017-05-23 21:47:58 -0700

    Update random_32.ml

--- miscellaneous/rosetta/random_32.ml
+++ miscellaneous/rosetta/random_32.ml
@@ -1,5 +1,6 @@
 (* 32 bit random number in ocaml *)
 (* run as ocaml random_32.ml *)
+(* turns out ocaml can only random max to 30 bits *)
 
 let myRan = (Random.int 2147483647) in 
   print_int myRan
